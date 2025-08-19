f5b88f347823e70ac8d9a5c9b94254aa699f9340
Ali Amir <codepirate2028@gmail.com>
2025-08-18 23:39:18 -0400

    Create proj3.cpp

---
+++ proj3.cpp
@@ -0,0 +1,15 @@
+//Title: proj3.cpp
+//Author: Ali Amir
+//Date: 3/12/2025
+
+#include "TextEditor.h"
+#include "Document.h"
+#include <iostream>
+using namespace std;
+
+int main() {
+    TextEditor T = TextEditor(); //Creates a new TextEditor object
+    T.Start(); //Calls the Start function
+    return 0;
+   
+}
